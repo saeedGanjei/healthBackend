9ab88d74e0fd6fb302f7f423fbfb3ac45258ebef
milad seyfi <miladseify5@gmail.com>
2023-05-23 16:20:37 +0330

    change db

--- health/health/settings.py
+++ health/health/settings.py
@@ -81,13 +81,9 @@ WSGI_APPLICATION = 'health.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "health",
-        "USER": "milad",
-        "PASSWORD": config("PASS_POSTGRES"),
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
