1772a052d83c93d537f28beb595beb6ca7d65dad
milad seyfi <miladseify5@gmail.com>
2023-05-23 16:47:22 +0330

    f

--- health/health/urls.py
+++ health/health/urls.py
@@ -30,9 +30,10 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-docs'),
     path('api/user/', include('user.urls')),
-]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
+] +  static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+
+
+# if settings.DEBUG:
+    # urlpatterns += static(settings.MEDIA_URL,
+                        #   document_root=settings.MEDIA_ROOT)
