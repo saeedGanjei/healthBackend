b53a3f3e01d984eab27f849ee23e71df3d469edb
milad seyfi <miladseify5@gmail.com>
2023-05-23 16:56:34 +0330

    f

--- health/health/settings.py
+++ health/health/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-from decouple import config
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +22,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-izo$12%l3lggtp24+c%x)s@rm(27_)-2p70u)#%6m@v4#@jjoy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
